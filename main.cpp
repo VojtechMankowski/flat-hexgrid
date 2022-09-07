fcec59ac989f7ab5117027f77a649275444a8939
VojtechMankowski <vojtaman@gmail.com>
2022-09-07 21:23:15 +0200

    Added FPS limiting

--- main.cpp
+++ main.cpp
@@ -5,24 +5,40 @@
 
 
 const int WIDTH = 800, HEIGHT = 600;
+const int FPS = 60;
+const int frameDelay = 1000 / FPS;  // [ms]
 
 int main(int argc, char* argv[]) {
     // std::ios_base::sync_with_stdio(0);
     // std::cin.tie(0);
- 
+
+    unsigned int frameStart = 0;
+    int frameTicks = 0;
+
     Game game(WIDTH, HEIGHT);
     game.init("Tell Your Tale", WIDTH, HEIGHT);
 
     game.loadResources(); // initStage
 
     while (game.running()) {  // Input, Update, Draw
+
+        frameStart = SDL_GetTicks();
+
         // Input
         game.prepareScene();
         game.handleInput();
         game.update();
         game.draw();
 
-        SDL_Delay(16);
+        // Limit FPS
+        frameTicks = SDL_GetTicks() - frameStart;
+
+        if (frameDelay > frameTicks) {
+            SDL_Delay(frameDelay - frameTicks);
+        }
+        
+        frameTicks = SDL_GetTicks() - frameStart;
+        printf("%d ms, ( %.2f FPS)\n", frameTicks, 1000.0f/frameTicks );
     }
 
     game.quit();
